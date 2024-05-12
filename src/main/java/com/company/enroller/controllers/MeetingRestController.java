094472c92e435bd3aac845a3345ec2bae0ce5eba
JustynaHerczek <justynaherczek@gmail.com>
2024-05-12 19:34:47 +0200

    added basic CRUD operations

--- src/main/java/com/company/enroller/controllers/MeetingRestController.java
+++ src/main/java/com/company/enroller/controllers/MeetingRestController.java
@@ -1,14 +1,11 @@
 package com.company.enroller.controllers;
 
-import com.company.enroller.model.Participant;
+import com.company.enroller.model.Meeting;
 import com.company.enroller.persistence.MeetingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import com.company.enroller.model.Meeting;
-import com.company.enroller.persistence.MeetingService;
 
 import java.util.Collection;
 
@@ -16,21 +13,17 @@ import java.util.Collection;
 @RequestMapping("/meetings")
 public class MeetingRestController {
 
-
     @Autowired
     MeetingService meetingService;
 
     @RequestMapping(value = "", method = RequestMethod.GET)
-    public ResponseEntity<Collection<Meeting>> getAll() {
+    public ResponseEntity<Collection<Meeting>> getAllMeetings() {
         Collection<Meeting> allMeetings = meetingService.getAll();
-        if (allMeetings != null) {
-            return ResponseEntity.ok(allMeetings);
-        } else return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-
+        return ResponseEntity.ok(allMeetings);
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
-    public ResponseEntity<Meeting> getMeeting(@PathVariable("id") long id) {
+    public ResponseEntity<Meeting> getMeetingById(@PathVariable("id") long id) {
         Meeting meeting = meetingService.findByID(id);
         if (meeting == null) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
@@ -38,4 +31,35 @@ public class MeetingRestController {
         return new ResponseEntity<>(meeting, HttpStatus.OK);
     }
 
+    @RequestMapping(value = "", method = RequestMethod.POST)
+    public ResponseEntity<?> addMeeting(@RequestBody Meeting meeting) {
+        if (meetingService.findByID(meeting.getId()) != null) {
+            return new ResponseEntity<>("\n" + "Nie można utworzyć. Spotkanie z identyfikatorem " + meeting.getId() + " już istnieje.", HttpStatus.CONFLICT);
+        }
+        meetingService.add(meeting);
+        return new ResponseEntity<>(meeting, HttpStatus.CREATED);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+    public ResponseEntity<?> updateMeeting(@PathVariable("id") long id, @RequestBody Meeting updatedMeeting) {
+        Meeting meeting = meetingService.findByID(id);
+        if (meeting == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        meeting.setTitle(updatedMeeting.getTitle());
+        meeting.setDescription(updatedMeeting.getDescription());
+        meeting.setDate(updatedMeeting.getDate());
+        meetingService.update(meeting);
+        return new ResponseEntity<>(meeting, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<?> deleteMeeting(@PathVariable("id") long id) {
+        Meeting meeting = meetingService.findByID(id);
+        if (meeting == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        meetingService.delete(meeting);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
 }
--- src/main/java/com/company/enroller/persistence/MeetingService.java
+++ src/main/java/com/company/enroller/persistence/MeetingService.java
@@ -1,31 +1,75 @@
 package com.company.enroller.persistence;
 
 import com.company.enroller.model.Meeting;
-import com.company.enroller.model.Participant;
 import org.hibernate.Session;
-import org.hibernate.query.Query;
+import org.hibernate.Transaction;
 import org.springframework.stereotype.Component;
 
-import javax.net.ssl.HandshakeCompletedEvent;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 import java.util.Collection;
 
 @Component("meetingService")
 public class MeetingService {
 
-	Session session;
+	private Session session;
 
 	public MeetingService() {
-		session = DatabaseConnector.getInstance().getSession();
+		this.session = DatabaseConnector.getInstance().getSession();
 	}
 
 	public Collection<Meeting> getAll() {
-		String hql = "FROM Meeting";
-		Query query = this.session.createQuery(hql);
-		return query.list();
+		CriteriaBuilder builder = session.getCriteriaBuilder();
+		CriteriaQuery<Meeting> criteria = builder.createQuery(Meeting.class);
+		Root<Meeting> root = criteria.from(Meeting.class);
+		criteria.select(root);
+		return session.createQuery(criteria).getResultList();
 	}
-
 
 	public Meeting findByID(long id) {
 		return session.get(Meeting.class, id);
+	}
+
+	public void add(Meeting meeting) {
+		Transaction transaction = null;
+		try {
+			transaction = session.beginTransaction();
+			session.save(meeting);
+			transaction.commit();
+		} catch (Exception e) {
+			if (transaction != null) {
+				transaction.rollback();
+			}
+			e.printStackTrace();
+		}
+	}
+
+	public void update(Meeting meeting) {
+		Transaction transaction = null;
+		try {
+			transaction = session.beginTransaction();
+			session.update(meeting);
+			transaction.commit();
+		} catch (Exception e) {
+			if (transaction != null) {
+				transaction.rollback();
+			}
+			e.printStackTrace();
+		}
+	}
+
+	public void delete(Meeting meeting) {
+		Transaction transaction = null;
+		try {
+			transaction = session.beginTransaction();
+			session.delete(meeting);
+			transaction.commit();
+		} catch (Exception e) {
+			if (transaction != null) {
+				transaction.rollback();
+			}
+			e.printStackTrace();
+		}
 	}
 }
